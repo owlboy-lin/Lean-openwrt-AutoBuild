de85eecb1364f6cab09ead0bed8dbf9e899e3021
owlboy-lin <298076473@qq.com>
2024-04-14 21:02:07 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,7 +20,7 @@ sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 
 # echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages;master' >>feeds.conf.default
 
