0bf4f5051d4829d33d9045db59d1fd4491a1201e
owlboy-lin <298076473@qq.com>
2024-04-20 15:38:15 +0800

    update

--- rpi-4B/rpi-4B-2.sh
+++ rpi-4B/rpi-4B-2.sh
@@ -66,35 +66,6 @@
 # 科学上网插件依赖
 
 
-
-# openclash
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-openclash  package/luci-app-openclash
-# 加入OpenClash核心
-# chmod -R a+x $GITHUB_WORKSPACE/preset-clash-core.sh
-# if [ "$1" = "rk33xx" ]; then
-#     $GITHUB_WORKSPACE/preset-clash-core.sh arm64
-# elif [ "$1" = "rk35xx" ]; then
-#     $GITHUB_WORKSPACE/preset-clash-core.sh arm64
-# elif [ "$1" = "x86" ]; then
-#     $GITHUB_WORKSPACE/preset-clash-core.sh amd64
-# fi
-
-# adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-svn export https://github.com/kenzok8/openwrt-packages/trunk/adguardhome package/adguardhome
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-adguardhome package/luci-app-adguardhome
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/adguardhome package/adguardhome
-
-# mosdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/mosdns package/mosdns
-svn export https://github.com/kenzok8/openwrt-packages/trunk/v2dat package/v2dat
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/luci-app-mosdns package/luci-app-mosdns
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/mosdns package/mosdns
-# svn export https://github.com/kiddin9/openwrt-packages/trunk/v2dat package/v2dat
-
-
 # 添加自定义软件包
 
 echo "
@@ -113,100 +84,6 @@ CONFIG_TARGET_ROOTFS_PARTSIZE=160
 
 # ipv6
 CONFIG_PACKAGE_ipv6helper=y
-
-
-# airplay2
-CONFIG_PACKAGE_luci-app-airplay2 is not set
-CONFIG_PACKAGE_luci-i18n-airplay2-zh-cn is not set
-
-
-# 自动重启
-CONFIG_PACKAGE_luci-app-autoreboot=y
-CONFIG_PACKAGE_luci-i18n-autoreboot-zh-cn=y
-
-# # Themes
-CONFIG_PACKAGE_luci-app-argon-config=y
-CONFIG_PACKAGE_luci-app-argone-config=y
-CONFIG_PACKAGE_luci-i18n-argon-config-zh-cn=y
-CONFIG_PACKAGE_luci-i18n-argone-config-zh-cn=y
-CONFIG_PACKAGE_luci-theme-argone=y
-
-# 关机
-CONFIG_PACKAGE_luci-app-poweroff=y
-CONFIG_PACKAGE_luci-i18n-poweroff-zh-cn=y
-
-# openclash
-CONFIG_PACKAGE_luci-app-openclash=y
-CONFIG_PACKAGE_luci-i18n-openclash-zh-cn=y
-
-# adguardhome
-CONFIG_PACKAGE_luci-app-adguardhome=y
-CONFIG_PACKAGE_luci-app-adguardhome_INCLUDE_binary=y
-CONFIG_PACKAGE_luci-i18n-adguardhome-zh-cn=y
-
-# mosdns
-CONFIG_PACKAGE_luci-app-mosdns=y
-CONFIG_PACKAGE_luci-i18n-mosdns-zh-cn=y
-
-# netspeedtest chmod +x /etc/init.d/netspeedtest
-CONFIG_PACKAGE_luci-app-netspeedtest=y
-CONFIG_PACKAGE_luci-i18n-netspeedtest-zh-cn=y
-
-# passwall
-CONFIG_PACKAGE_luci-app-passwall=y
-CONFIG_PACKAGE_luci-i18n-passwall-zh-cn=y
-
-CONFIG_PACKAGE_luci-app-passwall2=y
-CONFIG_PACKAGE_luci-i18n-passwall2-zh-cn=y
-
-# VPN
-CONFIG_PACKAGE_luci-app-ipsec-vpnd=y
-CONFIG_PACKAGE_luci-i18n-ipsec-vpnd-zh-cn=y
-
-CONFIG_PACKAGE_luci-app-openvpn=y
-CONFIG_PACKAGE_luci-i18n-openvpn-zh-cn=y
-
-# quickstart
-CONFIG_PACKAGE_luci-app-quickstart=y
-CONFIG_PACKAGE_luci-i18n-quickstart-zh-cn=y
-
-# store
-CONFIG_PACKAGE_luci-app-store=y
-
-# luci-app-ttyd=y
-CONFIG_PACKAGE_luci-app-ttyd=y
-CONFIG_PACKAGE_luci-i18n-ttyd-zh-cn=y
-
-CONFIG_PACKAGE_luci-app-unblockmusic=y
-CONFIG_PACKAGE_luci-app-unblockmusic_INCLUDE_UnblockNeteaseMusic_Go=y
-CONFIG_PACKAGE_luci-app-unblockmusic_INCLUDE_UnblockNeteaseMusic_NodeJS=y
-CONFIG_PACKAGE_luci-i18n-unblockmusic-zh-cn=y
-
-# luci-app-uugamebooster=y
-CONFIG_PACKAGE_luci-app-uugamebooster=y
-CONFIG_PACKAGE_luci-i18n-uugamebooster-zh-cn=y
-
-
-# luci-app-webadmin=y
-CONFIG_PACKAGE_luci-app-webadmin=y
-CONFIG_PACKAGE_luci-i18n-webadmin-zh-cn=y
-
-# rclone
-CONFIG_PACKAGE_rclone=y
-CONFIG_PACKAGE_fuse3-utils=y
-
-# 删除不用的插件
-CONFIG_PACKAGE_autosamba=y
-CONFIG_PACKAGE_luci-app-accesscontrol=y
-CONFIG_PACKAGE_luci-i18n-accesscontrol-zh-cn=y
-CONFIG_PACKAGE_luci-app-upnp=y
-CONFIG_PACKAGE_luci-i18n-upnp-zh-cn=y
-CONFIG_PACKAGE_luci-app-vlmcsd=y
-CONFIG_PACKAGE_luci-i18n-vlmcsd-zh-cn=y
-CONFIG_PACKAGE_luci-app-vsftpd=y
-CONFIG_PACKAGE_luci-i18n-vsftpd-zh-cn=y
-CONFIG_PACKAGE_luci-app-samba4=y
-CONFIG_PACKAGE_luci-i18n-samba4-zh-cn=y
 
 " >> .config
 
