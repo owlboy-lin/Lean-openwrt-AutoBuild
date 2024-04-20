867f006365ccefb1638c0f8a16a702076e2694e9
owlboy-lin <298076473@qq.com>
2024-04-20 19:29:21 +0800

    update

--- RT_ACRH17/RT_ACRH17-1.sh
+++ RT_ACRH17/RT_ACRH17-1.sh
@@ -56,10 +56,10 @@
 
 #  下载源码
 ## adguardhome
-# git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 
 ## autoreboot
-git clone https://github.com/f8q8/luci-app-autoreboot package/luci-app-autoreboot
+# git clone https://github.com/f8q8/luci-app-autoreboot package/luci-app-autoreboot
 
 ## mosdns
 echo 'src-git mosdns https://github.com/sbwml/luci-app-mosdns' >>feeds.conf.default
